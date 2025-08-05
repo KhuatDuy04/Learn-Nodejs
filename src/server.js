65dccdb8bbfa61879b6ba6cac2389f8a5a97eff5
Duykiller04 <haijano04@gmail.com>
2025-08-05 09:34:37 +0700

    connection database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express'); //commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web')
+const pool = require('./config/database');
 
 const app = express(); // app express
 const port = process.env.PORT || 3001; // port => hardcode
@@ -12,6 +13,17 @@ configViewEngine(app)
 
 //Khai báo route
 app.use('/', webRoutes)
+
+async function testDb() {
+  try {
+    const [results, fields] = await pool.query('SELECT * FROM Users u');
+    console.log('>>>results =', results);
+  } catch (err) {
+    console.error('DB error:', err);
+  }
+}
+
+testDb();
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
